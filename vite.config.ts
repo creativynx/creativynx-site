3ee5e2601ed112c1a9c4da1e893a6ab6e8295d13
Praveen Siva <praveensiva0820@gmail.com>
2026-01-02 11:43:03 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/creativynx-site/", // Base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
